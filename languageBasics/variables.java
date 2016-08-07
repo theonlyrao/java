a68186970e84b273f4bac77045e3d8217fce91e9
Ashwin Rao <theonlyrao@gmail.com>
2016-08-06 19:05:42 -0600

    Complete basic variable section in language basics

---
+++ languageBasics/variables.java
@@ -0,0 +1,14 @@
+class Variables {
+    public static void main(String[] args) {
+	int num;
+	num = 10;
+	System.out.println("num is " + num);
+
+	char pizza = 'c';
+	System.out.println("pizza is " + pizza);
+
+	String word = "hello";
+	System.out.println("word is " + word);
+	
+    }
+}
