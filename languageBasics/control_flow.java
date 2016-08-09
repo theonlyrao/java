0bb7a63eceb1da9ebdc2f2631beb85545fa5d0a2
Ashwin Rao <theonlyrao@gmail.com>
2016-08-09 10:12:17 -0600

    Complete control flow lessons and exercises

---
+++ languageBasics/control_flow.java
@@ -0,0 +1,16 @@
+class ControlFlow {
+    public static void main (String[] args) {
+	short aNumber = 3;
+	
+	if (aNumber >= 0) {
+	    if (aNumber == 0) {
+		System.out.println("first string");
+	    }
+	    else {
+		System.out.println("second string");
+	    }
+	}
+
+	System.out.println("third string");
+    }
+}
