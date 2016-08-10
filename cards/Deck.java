70a34b275bef3126d575a176864ea18646f2a4f0
Ashwin Rao <theonlyrao@gmail.com>
2016-08-10 09:32:06 -0600

    Update Deck with instance methods

--- cards/Deck.java
+++ cards/Deck.java
@@ -1,15 +1,19 @@
 public class Deck {
     private static int numberOfCards = 52;
 
-    private static Card[] cardsInDeck = new Card[52];
+    private Card[] cardsInDeck = new Card[numberOfCards];
 
-    //public Deck() {
-	static {
+    public Deck(String type) {
+	if ("jacks".equals(type)) {
 	    for(int i = 0; i < numberOfCards; i++){
 		cardsInDeck[i] = new Card("jack", "diamonds");
 	    }
+	} else {
+	    for(int i = 0; i < numberOfCards; i++){
+		cardsInDeck[i] = new Card("ace", "spades");
+	    }
 	}
-    //}
+    }
 
     public String showTopCard(){
 	return this.cardsInDeck[0].getRank() + " of " + this.cardsInDeck[0].getSuite();
--- cards/makeCard.java
+++ cards/makeCard.java
@@ -1,7 +1,6 @@
 public class makeCard {
     public static void main(String[] args){
 	Card aceOfSpades = new Card("ace", "spades");
-	//assert aceOfSpades.getSuite() == "spades";
 	System.out.println("card suite is " + aceOfSpades.getSuite());
 
 	Deck myDeck = new Deck();
