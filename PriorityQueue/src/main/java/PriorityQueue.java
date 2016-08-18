9c96314cfe9582a2c4902e6f7f11ac7778d059a0
Ashwin Rao <theonlyrao@gmail.com>
2016-08-18 16:26:51 +0530

    Get deleteMin() test to pass
    Create new Node class that has field of "parent" and do minimum to have
    it return the right thing.
    
    This implies design choice of having a Node instance that contains the
    parent, which is probably not the right term. Next version will replace
    parent with "data", "childOne", and "childTwo".

--- PriorityQueue/src/main/java/PriorityQueue.java
+++ PriorityQueue/src/main/java/PriorityQueue.java
@@ -3,5 +3,10 @@ class PriorityQueue {
     public PriorityQueue(String unorderedString){
 	String original = unorderedString;
     }
+
+    public String deleteMin(){
+	Node newNode = new Node();	
+	return newNode.parent;
+    }
 }
 
--- PriorityQueue/src/test/java/PriorityQueueTest.java
+++ PriorityQueue/src/test/java/PriorityQueueTest.java
@@ -4,6 +4,6 @@ import static org.junit.Assert.*;
 public class PriorityQueueTest {
     @Test public void insert() {
         PriorityQueue classUnderTest = new PriorityQueue("PRIORITY");
-        assertTrue("IIOPRRTY", classUnderTest.deleteMin());
+        assertEquals("IIOPRRTY", classUnderTest.deleteMin());
     }
 }
