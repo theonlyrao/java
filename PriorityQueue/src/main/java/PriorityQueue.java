a27d7b6da0802ad7851ef6246810193f878f6c4f
Ashwin Rao <theonlyrao@gmail.com>
2016-08-19 14:29:32 +0530

    Implement alpha => index constant map for class
    Taken from
    http://stackoverflow.com/questions/507602/how-can-i-initialise-a-static-map. If
    future implementation wants to let a user set the priority of the
    letters, i.e. make the values of this map public, then this
    implementation might need to be changed.

--- PriorityQueue/src/main/java/PriorityQueue.java
+++ PriorityQueue/src/main/java/PriorityQueue.java
@@ -1,11 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Collections;
+
 class PriorityQueue {
+    private static final Map<String, Integer> alphabet;
+    static {
+	Map<String, Integer> myMap = new HashMap<String, Integer>();
+	myMap.put("A", 1);
+	myMap.put("B", 2);
+	alphabet = Collections.unmodifiableMap(myMap);
+    }
+    
+    
 
     public PriorityQueue(String unorderedString){
 	String original = unorderedString;
     }
 
     public Node min(){
-	Node minNode = new Node("I", "I", "O");
+	Node minNode = new Node("A", "B", "C");
 	return minNode;
     }
 
